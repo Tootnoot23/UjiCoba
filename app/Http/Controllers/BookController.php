7a1586641145a0097a1ab7c63e064edd57bf8ff5
Tootnoot23 <tora.winarto@binus.ac.id>
2022-05-23 16:29:18 +0700

    Initial project

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use App\Http\Requests\StoreBookRequest;
+use App\Http\Requests\UpdateBookRequest;
+
+class BookController extends Controller
+{
+
+    public function index()
+    {
+        return view('welcome');
+    }
+
+    public function create(Request $request)
+    {
+        $nama = $request->file('gambar')->getClientOriginalName();
+        $gambar = $request->file('gambar');
+        Storage::put($gambar, $nama);
+        // Book::create([
+        //     'Title' => $request->Title_book,
+        //     'Author' => $request->Author_book,
+        //     'Publisher' => $request->Publisher_book,
+        //     'Year' => $request->Year_book,
+        // ]);
+        $validated = $request->validate([
+            'Title' => ['required', 'string', 'between:1,10'],
+            'Author' => ['required'],
+            'Publisher' => ['required'],
+            'Year' => ['required'],
+            'Gambar' => ['required'],
+        ], [
+            'Title.required' => 'the title must be filled'
+        ]);
+        if ($validated) {
+            Book::create([
+                'Title' => $request->Title,
+                'Author' => $request->Author,
+                'Publisher' => $request->Publisher,
+                'Year' => $request->Year,
+                'Gambar' => $nama,
+            ]); 
+            return view('welcome');
+        }
+        return back();
+    }
+
+    public function show(Book $book)
+    {
+        $books = Book::all();
+        return view('list', compact('books'));
+    }
+
+    public function edit($id, Request $request)
+    {
+        $book = Book::findOrFail($id);
+        return view('update', compact('book'));
+    }
+
+    public function update($id, Request $request, Book $book)
+    {
+        Book::findOrFail($id)->update([
+            'Title' => $request->Title_book,
+            'Author' => $request->Author_book,
+            'Publisher' => $request->Publisher_book,
+            'Year' => $request->Year_book,
+        ]);
+        $books = Book::all();
+        return view('list', compact('books'));
+    }
+
+    public function destroy($id)
+    {
+        Book::destroy($id);
+        return back();
+    }
+}
